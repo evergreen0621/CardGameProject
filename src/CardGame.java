497f66bf432e4b530c6d5ced9cb8f5bdf1033991
SangRokChoi <sangnong1324@naver.com>
2024-05-02 19:24:38 +0900

    카드게임 클래스 수정

--- src/CardGame.java
+++ src/CardGame.java
@@ -19,7 +19,7 @@ public class CardGame {
             if(choice == 1){
                 // 게임하기 선택 시
                 while(true){
-                    System.out.println("지금 플레이하실 수 있는 게임들은 다음과 같습니다.\n어떤 게임을 하시겠습니까?\n1. 카지노 워 (Casino War)\n2. 업 다운 (Up & Down) \n3. 세븐 (Sevens7) \n4. 오마하 포커 (Omaha Poker) \n5. 숫자야구 (Baseball Game)\n6. 스파이더 솔리테어 (Spider Solitaire)\n");
+                    System.out.println("지금 플레이하실 수 있는 게임들은 다음과 같습니다.\n어떤 게임을 하시겠습니까?\n1. 카지노 워 (Casino War)\n2. 업 다운 (Up & Down) \n3. 세븐 (Sevens7) \n4. 오마하 포커 (Omaha Poker) \n5. 숫자야구 (Baseball Game)\n6. 스파이더 솔리테어 (Spider Solitaire)");
                 
                     int gamechoice = sc.nextInt();
 
@@ -30,9 +30,8 @@ public class CardGame {
                         break;
                     }
                     else if(gamechoice == 2){
-                        System.out.println(" 업다운 카드게임이 시작됩니다 !!");
+                        System.out.println("업다운 카드게임이 시작됩니다 !!");
                         UpDownCardGame.main(args);
-                        
                         // 2번 게임 선택 시
                         break;
                     }
@@ -45,20 +44,19 @@ public class CardGame {
                     else if(gamechoice == 4){
                         System.out.println("오마하 포커 카드게임이 시작됩니다 !!");
                         OmahaPoker.main(args);
-                        // 3번 게임 선택 시
+                        // 4번 게임 선택 시
                         break;
                     }
                     else if(gamechoice == 5){
                         System.out.println("야구 카드게임이 시작됩니다 !!");
                         BaseballGame.main(args);
-                        // 3번 게임 선택 시
+                        // 5번 게임 선택 시
                         break;
                     }
                     else if(gamechoice == 6){
-                        
                         System.out.println("스파이더 솔리테어 게임이 시작됩니다 !!");
                         SpiderSolitaire.main(args);
-                        // 3번 게임 선택 시
+                        // 6번 게임 선택 시
                         break;
                     }
                     else{
@@ -76,7 +74,6 @@ public class CardGame {
                 System.out.println("4번 게임 결과:");
                 System.out.println("5번 게임 결과:");
                 System.out.println("6번 게임 결과:");
-                
                 System.out.println("");
                 System.out.println("총 합산 점수:");
                 System.out.println("");
